2ff9bde665a9bb127b6657a5d030427e1a880847
ilesanmi olushola <ilesanmiolushola9@gmail.com>
2024-07-09 17:06:50 +0100

    add new commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -13,6 +13,7 @@ import { useEffect, useState } from "react";
 
 const schema = yup.object().shape({
   name: yup.string().required("This is a required field"),
+  g_recaptcha_response: yup.string(),
   username: yup.string().required("This is a required field"),
   useEmail: yup.boolean().required(),
   email: yup
@@ -37,7 +38,8 @@ const siteKey = import.meta.env.VITE_SITE_KEY;
 const Register = () => {
   const navigate = useNavigate();
   const [isCaptchaVerified, setIsCaptchaVerified] = useState(false);
-  const [useEmail, setUseEmail] = useState(false); // Default to phone number
+  const [useEmail, setUseEmail] = useState(null); // Initially, no form is shown
+  const [captchaResponse, setCaptchaResponse] = useState(""); // State to store captcha response
 
   const [registers, { isLoading }] = useRegistersMutation();
 
@@ -55,11 +57,14 @@ const Register = () => {
   });
 
   useEffect(() => {
-    setValue("useEmail", useEmail);
+    if (useEmail !== null) {
+      setValue("useEmail", useEmail);
+    }
   }, [useEmail, setValue]);
 
-  const handleCaptchaVerification = () => {
+  const handleCaptchaVerification = (response) => {
     setIsCaptchaVerified(true);
+    setCaptchaResponse(response); // Capture captcha response
   };
 
   const [referralNumber, setReferralNumber] = useState(null);
@@ -79,13 +84,14 @@ const Register = () => {
     }
   }, []);
 
-  const switchRegistrationMethod = () => {
-    if (useEmail) {
-      resetField("email");
-    } else {
+  const switchRegistrationMethod = (method) => {
+    if (method === "email") {
       resetField("phone");
+      setUseEmail(true);
+    } else {
+      resetField("email");
+      setUseEmail(false);
     }
-    setUseEmail(!useEmail);
   };
 
   const submitForm = async (data) => {
@@ -96,6 +102,7 @@ const Register = () => {
       if (referralNumber) {
         data.user = referralNumber;
       }
+      data.g_recaptcha_response = captchaResponse; 
       const res = await registers(data).unwrap();
       if (res) {
         toast.success(
@@ -123,148 +130,166 @@ const Register = () => {
       <div className="container mb-5">
         <div className="row">
           <div className="col-lg-6 mx-auto mb-5 app__register">
-            <h1 className="mb-4">
-              Register {useEmail ? "with Email Address" : "with Phone Number"}
+            <h1 className="mb-4 text-center">
+              Register{" "}
+              {useEmail === null
+                ? ""
+                : useEmail
+                ? "with Email Address"
+                : "with Phone Number"}
             </h1>
+
             <p className="mb-4 text-center">
               Play all your favorite Nigerian lotto games from one account on
               mylottohub and get your winnings paid instantly to your bank
               account
             </p>
-
-            <form onSubmit={handleSubmit(submitForm)} autoComplete="off">
-              <div className="mb-3">
-                <input
-                  type="text"
-                  className="form-control mb-2 p-3"
-                  placeholder="Full Name"
-                  name="name"
-                  {...register("name", {
-                    required: "Required",
-                  })}
-                />
-                {errors.name && (
-                  <p className="text-danger text-capitalize">
-                    {errors.name.message}
-                  </p>
-                )}
-              </div>
-
-              <div className="mb-3">
-                <input
-                  type="text"
-                  className="form-control mb-2 p-3"
-                  placeholder="Username"
-                  name="username"
-                  {...register("username", {
-                    required: "Required",
-                  })}
-                />
-                {errors.username && (
-                  <p className="text-danger text-capitalize">
-                    {errors.username.message}
-                  </p>
-                )}
-              </div>
-
-              {useEmail ? (
-                <div className="mb-3">
-                  <input
-                    type="email"
-                    className="form-control mb-2 p-3"
-                    placeholder="Email Address"
-                    name="email"
-                    {...register("email")}
-                  />
-                  {errors.email && (
-                    <p className="text-danger text-capitalize">
-                      {errors.email.message}
-                    </p>
-                  )}
-                </div>
-              ) : (
-                <div className="mb-3">
-                  <input
-                    type="number"
-                    min={1}
-                    className="form-control mb-2 p-3"
-                    placeholder="Phone Number"
-                    name="phone"
-                    {...register("phone")}
-                    onInput={(e) =>
-                      (e.target.value = e.target.value.slice(0, 11))
-                    }
-                  />
-                  {errors.phone && (
-                    <p className="text-danger text-capitalize">
-                      {errors.phone.message}
-                    </p>
-                  )}
-                </div>
-              )}
-
-              <Button
-                variant="primary"
-                onClick={switchRegistrationMethod}
-                className="mb-3"
-              >
-                {useEmail
-                  ? "Use Phone Number for Registration"
-                  : "Use Email for Registration"}
-              </Button>
-
-              <div className="mb-3 form-check mt-4">
-                <input type="checkbox" className="form-check-input" required />
-                <label className="form-check-label">
-                  I have read the{" "}
-                  <a
-                    className="text-primary"
-                    onClick={() => {
-                      navigate(`/terms`);
-                    }}
-                    style={{ textDecoration: "none", cursor: "pointer" }}
+            {useEmail === null ? (
+              <>
+                <div className="d-flex mt-5 justify-content-between mb-4">
+                  <Button
+                    variant="primary"
+                    onClick={() => switchRegistrationMethod("phone")}
+                    className="me-2"
                   >
-                    Terms and Conditions
-                  </a>
-                </label>
-              </div>
-
-              <p
-                onClick={() =>
-                  (window.location.href =
-                    "https://api.mpin.io/authorize?client_id=v8kfysqoljbgd&response_type=code&scope=openid+email+profile&redirect_uri=https://mylottohub.com")
-                }
-                style={{ cursor: "pointer", color: "#406777" }}
-              >
-                Already have an account?
-                <span className="text-primary">Sign in</span>
-              </p>
-              <ReCaptchaV2
-                sitekey={siteKey}
-                onChange={handleCaptchaVerification}
-              />
-              <Button
-                type="submit"
-                className="w-100 p-3 mt-3"
-                disabled={!isCaptchaVerified || isLoading}
-              >
-                {isLoading ? (
-                  <Spinner
-                    as="span"
-                    animation="border"
-                    size="sm"
-                    role="status"
-                    aria-hidden="true"
+                    Register With Phone Number
+                  </Button>
+                  <Button
+                    variant="secondary"
+                    onClick={() => switchRegistrationMethod("email")}
+                  >
+                    Register With Email Address
+                  </Button>
+                </div>
+              </>
+            ) : (
+              <form onSubmit={handleSubmit(submitForm)} autoComplete="off">
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    className="form-control mb-2 p-3"
+                    placeholder="Full Name"
+                    name="name"
+                    {...register("name", {
+                      required: "Required",
+                    })}
                   />
+                  {errors.name && (
+                    <p className="text-danger text-capitalize">
+                      {errors.name.message}
+                    </p>
+                  )}
+                </div>
+
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    className="form-control mb-2 p-3"
+                    placeholder="Username"
+                    name="username"
+                    {...register("username", {
+                      required: "Required",
+                    })}
+                  />
+                  {errors.username && (
+                    <p className="text-danger text-capitalize">
+                      {errors.username.message}
+                    </p>
+                  )}
+                </div>
+
+                {useEmail ? (
+                  <div className="mb-3">
+                    <input
+                      type="email"
+                      className="form-control mb-2 p-3"
+                      placeholder="Email Address"
+                      name="email"
+                      {...register("email")}
+                    />
+                    {errors.email && (
+                      <p className="text-danger text-capitalize">
+                        {errors.email.message}
+                      </p>
+                    )}
+                  </div>
                 ) : (
-                  "Register"
+                  <div className="mb-3">
+                    <input
+                      type="number"
+                      min={1}
+                      className="form-control mb-2 p-3"
+                      placeholder="Phone Number"
+                      name="phone"
+                      {...register("phone")}
+                      onInput={(e) =>
+                        (e.target.value = e.target.value.slice(0, 11))
+                      }
+                    />
+                    {errors.phone && (
+                      <p className="text-danger text-capitalize">
+                        {errors.phone.message}
+                      </p>
+                    )}
+                  </div>
                 )}
-              </Button>
-            </form>
+
+                <div className="mb-3 form-check mt-4">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    required
+                  />
+                  <label className="form-check-label">
+                    I have read the{" "}
+                    <a
+                      className="text-primary"
+                      onClick={() => {
+                        navigate(`/terms`);
+                      }}
+                      style={{ textDecoration: "none", cursor: "pointer" }}
+                    >
+                      Terms and Conditions
+                    </a>
+                  </label>
+                </div>
+
+                <p
+                  onClick={() =>
+                    (window.location.href =
+                      "https://api.mpin.io/authorize?client_id=v8kfysqoljbgd&response_type=code&scope=openid+email+profile&redirect_uri=https://mylottohub.com")
+                  }
+                  style={{ cursor: "pointer", color: "#406777" }}
+                >
+                  Already have an account?
+                  <span className="text-primary"> Sign in</span>
+                </p>
+                <ReCaptchaV2
+                  sitekey={siteKey}
+                  onChange={handleCaptchaVerification}
+                />
+                <Button
+                  type="submit"
+                  className="w-100 p-3 mt-3"
+                  disabled={!isCaptchaVerified || isLoading}
+                >
+                  {isLoading ? (
+                    <Spinner
+                      as="span"
+                      animation="border"
+                      size="sm"
+                      role="status"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    "Register"
+                  )}
+                </Button>
+              </form>
+            )}
           </div>
         </div>
       </div>
-
       <Footer />
     </>
   );
